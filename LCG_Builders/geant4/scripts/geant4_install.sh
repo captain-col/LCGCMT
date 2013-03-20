18ab9e440309f5a73746ee579332324c85daa13b
Clark McGrew <clark.mcgrew@stonybrook.edu>
2013-03-20 15:21:28 -0400

    Update GEANT4 Builder
    The geant4 builder provided with LCGCMT is rather out of date. This updates
    the builder, and also provides a geant4 interface class.

--- LCG_Builders/geant4/scripts/geant4_install.sh
+++ LCG_Builders/geant4/scripts/geant4_install.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
- 
-cd ${LCG_builddir}/${LCG_package_config_version}
-
-afsdir=`echo ${LCG_reldir} | sed -e  's,build,afs/cern.ch/sw/lcg/app,' | sed -e 's,scratch,afs/cern.ch/sw/lcg/app,' `
-
-mkdir -p ${afsdir}/${LCG_package_config_version}
-
-#if [ -d ${afsdir}/SEAL/${LCG_package_config_version}/src ]; then src=src; fi
-
-tar -cf - geant4  | (cd  ${afsdir}/${LCG_package_config_version}; tar -xf - )
+#
+# Script to handle a cmake configured package.  This has been
+# customized for geant4.
+#
+# Go to the build directory and run make.  The build directory must
+# match LOCAL_build in the config script.
+cd ${LCG_destbindir}/build
+make install
--- LCG_Builders/geant4/scripts/geant4_make.sh
+++ LCG_Builders/geant4/scripts/geant4_make.sh
@@ -1,32 +1,9 @@
 #!/bin/sh
-
-if test $LCG_NGT_SLT_NAME == "g4tags" ; then
-  
-   /bin/sh $LCG_package_root/scripts/geant4_cmake.sh  $*
-
-else
-
-
-   export XERCESCROOT=$VARXERCESCROOT
-   date
-   echo CLHEP_BASE_DIR=$CLHEP_BASE_DIR
-   echo XERCESCROOT=$XERCESCROOT
-   echo 
-
-   g++ -v
-   echo $PATH
-
-   echo "***********************************************"
-   echo "****************** USED TAGS ******************"
-   echo "***********************************************"
-   cat ${G4INSTALL}/gettags.txt
-   echo "***********************************************"
-
-
-   cd ${G4INSTALL}/source
-   ${geant4_pre_make_step}
-   make -j 4 
-   ${geant4_post_make_step}
-   date
-
-fi
+#
+# Script to handle a cmake configured package.  This has been
+# customized for geant4.
+#
+# Go to the build directory and run make.  The build directory must
+# match LOCAL_build in the config script.
+cd ${LCG_destbindir}/build
+make

2cbce40fc946660d7b521fe4a9e91efd5e041d48
Clark McGrew <clark.mcgrew@stonybrook.edu>
2013-03-20 15:23:25 -0400

    Remove a bunch of obsolete files.

--- LCG_Builders/geant4/scripts/geant4_config.bat
+++
@@ -1,6 +0,0 @@
-cd %LCG_builddir%
-if exist %LCG_tarfilename% (
-if not exist %LCG_package% mkdir %LCG_package%
-cd %LCG_package%
-tar xvfz ../%LCG_tarfilename%
-)
--- LCG_Builders/geant4/scripts/geant4_test.bat
+++
@@ -1,1 +0,0 @@
-sh -c "G4INSTALL=`cygpath -m $G4INSTALL` G4WORKDIR=`cygpath -m $G4WORKDIR` G4BIN=`cygpath -m $G4BIN` G4TMP=`cygpath -m $G4TMP` G4LIB=`cygpath -m $G4LIB` ../scripts/geant4_test.sh "

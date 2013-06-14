6e5b41abc3d6d1798c442bdedf6e4b1dd5ad8cc0
Clark McGrew <clark.mcgrew@stonybrook.edu>
2013-06-14 18:23:36 -0400

    Update the boost LCG_Builders.

--- LCG_Builders/boost/scripts/boost_config.sh
+++ LCG_Builders/boost/scripts/boost_config.sh
@@ -4,12 +4,5 @@ cd ${LCG_builddir}
 tar xvfz ${LCG_tarfilename}
 cd boost_${LCG_package_file_config_version}
 
-echo "using python : ${LCG_python_version_twodigit} : ${LCG_python_root} ;" > user-config.jam
-if [[ $LCG_setcpp11 == yes ]]; then
-echo 'using gcc : : :  <compileflags>-std=gnu++0x ;' >> user-config.jam
-fi
-if [[ $CMTCONFIG == i686-* ]]; then
-echo 'using gcc : : :  <linkflags>-m32 <compileflags>-m32 ;' >> user-config.jam
-fi
-cd tools/build/v2/
-./bootstrap.sh
+./bootstrap.sh --prefix=${LCG_destdir} \
+    --with-libraries=${LCG_boost_with_libraries}
--- LCG_Builders/boost/scripts/boost_install.sh
+++ LCG_Builders/boost/scripts/boost_install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 cd ${LCG_builddir}/boost_${LCG_package_file_config_version}
-${LCG_bjam_bin} ${LCG_boost_jam_opts} ${LCG_boost_compile_options} --toolset=${LCG_boost_toolset} --prefix=${LCG_destbindir} install --user-config=user-config.jam
+
+${LCG_bjam_bin} ${LCG_boost_jam_opts} ${LCG_boost_compile_options} \
+    --toolset=${LCG_boost_toolset} \
+    --prefix=${LCG_destbindir} install 
+
 mkdir ${LCG_destbindir}/data_files/
 find ./ -name "*.csv" -exec cp {} ${LCG_destbindir}/data_files/ \;

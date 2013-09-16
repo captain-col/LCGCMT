af7d302b8f78b761409fa3907e31e52c58389136
Clark McGrew <clark.mcgrew@stonybrook.edu>
2013-09-16 16:17:30 -0400

    Add log4cpp.  This is needed to compile GENIE.

---
+++ LCG_Builders/log4cpp/scripts/log4cpp_config.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# This is the directory where source will be untarred. 
+install_directory=log4cpp/${LCG_package_config_version}/${LCG_CMTCONFIG}
+
+# This is the directory the tar file will unpack the source into and
+# it's relative to the install_directory
+package_directory=log4cpp
+
+cd ${LCG_builddir}
+if [ ! -d ${install_directory} ]; then
+    mkdir -p ${install_directory}
+fi
+cd ${install_directory}
+
+if [ -d ${package_directory} ]; then
+    echo XXXX ${LCG_builddir}/${package_directory} already exists. 
+    echo XXXX Not unpacking and reconfiguring.  
+    echo XXXX Remove directory to force reconfiguration.  
+    exit 0
+fi
+
+tar xvfz ${LCG_builddir}/${LCG_tarfilename}
+cd ${package_directory}
+
+./configure --prefix=${LCG_extdir}/${install_directory} ${LCG_log4cpp_config_opts}
---
+++ LCG_Builders/log4cpp/scripts/log4cpp_make.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# This is the directory where source will be untarred. 
+install_directory=log4cpp/${LCG_package_config_version}/${LCG_CMTCONFIG}
+
+# This is the directory the tar file will unpack the source into and
+# it's relative to the install_directory
+package_directory=log4cpp
+
+# Go to the source directory.
+cd ${LCG_builddir}/${install_directory}/${package_directory}
+
+# Build the package.
+make
+

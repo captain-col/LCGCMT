ca35f43d766cb13205b6fba6bcf2c0cf1793f0b1
Clark McGrew <clark.mcgrew@stonybrook.edu>
2014-03-28 11:36:00 -0400

    Add some environment definitions to genie_install.sh.
    Right now, the definitions are not used, but are available for when they
    are needed.

--- LCG_Builders/genie/scripts/genie_install.sh
+++ LCG_Builders/genie/scripts/genie_install.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+# This is the directory where source will be untarred. 
+install_directory=genie/${LCG_package_config_version}/${LCG_CMTCONFIG}
+
+# This is the directory the tar file will unpack the source into and
+# it's relative to the install_directory
+package_directory=Genie-${LCG_package_config_version}
+
+# Go to the source directory.
+cd ${LCG_builddir}/${install_directory}/${package_directory}
+
 # Nothing to install.

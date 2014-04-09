57e22ac6870f8bf3f533f11ef358e3afb7779beb
Clark McGrew <clark.mcgrew@stonybrook.edu>
2014-04-09 10:03:04 -0400

    Don't use parallel builds!

--- LCG_Builders/ROOT/scripts/ROOT_make.sh
+++ LCG_Builders/ROOT/scripts/ROOT_make.sh
@@ -5,14 +5,14 @@ date
 # Set the maximum load during the job.  This can be overridden in the
 # environment.
 if [ "x$LCG_MAX_LOAD" = "x" ]; then
-    LCG_MAX_LOAD=2.5
+    LCG_MAX_LOAD=1.0
 fi
 echo Limit load to ${LCG_MAX_LOAD}
 
 cd ${LCG_destbindir}/${LCG_srcdir}
 
 # Build the main root package
-make -k -l ${LCG_MAX_LOAD} -j
+make -k -l ${LCG_MAX_LOAD} -j 1
 
 # Now build the tests.
 # cd test

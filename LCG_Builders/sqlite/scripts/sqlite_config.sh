75a47589dc766fc6d086fce30d1ac90df0349d6f
Clark McGrew <clark.mcgrew@stonybrook.edu>
2014-10-09 15:12:05 +0900

    Compile sqlite so we have a calibrations database.

--- LCG_Builders/sqlite/scripts/sqlite_config.sh
+++ LCG_Builders/sqlite/scripts/sqlite_config.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+package_directory=sqlite-autoconf-${LCG_package_config_version}
+
 cd ${LCG_builddir}
+if [ -d ${package_directory} ]; then
+    echo XXXX ${LCG_builddir}/${package_directory} already exists. 
+    echo XXXX Not unpacking and reconfiguring.  
+    echo XXXX Remove directory to force reconfiguration.  
+    exit 0
+fi
+
 tar xvfz ${LCG_tarfilename}
-cd sqlite-autoconf-${LCG_package_config_version}
-${LCG_sqlite_compile_options}
- ./configure --prefix=${LCG_destbindir}
+cd ${package_directory}
+
+CFLAGS="${LCG_extra_cflags}" ./configure --prefix=${LCG_extdir}/sqlite/${LCG_package_config_version}/${LCG_CMTCONFIG} ${LCG_sqlite_config_opts}
+

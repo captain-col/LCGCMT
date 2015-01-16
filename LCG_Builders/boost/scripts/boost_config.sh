8b9c0b5f11a5fcc4437ee850cbbb42a7636d4e78
Clark McGrew <clark.mcgrew@stonybrook.edu>
2015-01-15 17:30:07 -0700

    Don't unpack and reconfigure boost if it has already been configured
    The config script now checks to see if boost has been configured before
    trying to configure.  If it does need to be configured, the existing source
    should be deleted.

--- LCG_Builders/boost/scripts/boost_config.sh
+++ LCG_Builders/boost/scripts/boost_config.sh
@@ -5,6 +5,13 @@ package_directory=boost_${LCG_file_config_version}
 mkdir -p ${LCG_destbindir}
 cd ${LCG_destbindir}
 
+if [ -d ${package_directory} ]; then
+    echo XXXX ${LCG_destbindir}/${package_directory} already exists. 
+    echo XXXX Not unpacking and reconfiguring.  
+    echo XXXX Remove directory to force reconfiguration.  
+    exit 0
+fi
+
 tar xvfz ${LCG_tardir}/${LCG_tarfilename}
 cd ${package_directory}
 

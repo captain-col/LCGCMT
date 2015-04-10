59c0c9af068002be50032a924aeb639ce29f948e
Clark McGrew <clark.mcgrew@stonybrook.edu>
2015-04-10 14:17:10 -0400

    Make the scripts a bit more careful about not provoking recompilation.

--- LCG_Builders/geant4/scripts/geant4_config.sh
+++ LCG_Builders/geant4/scripts/geant4_config.sh
@@ -6,10 +6,8 @@
 # default is to install in the LCG_destbindir so that the build
 # directory is near the installation directory.  It needs to be the
 # LCG_destbindir since many autoconf and related packages compile in
-# the source directories.  Make sure the destination exists, and then
-# go there.
+# the source directories.  Make sure the destination exists.
 mkdir -p ${LCG_destdir}
-cd ${LCG_destdir}
 
 # This needs to be set to the installation prefix
 LOCAL_install=${LCG_destbindir}
@@ -24,23 +22,31 @@ mkdir -p ${LOCAL_build}
 # changes the top-level tar directory, this will need to be changed.
 LOCAL_src=${LCG_destdir}/${LCG_srcdir}
 
+# Unpack the geant4 data files
+if [ ! -d ${LCG_destdir}/../share ]; then
+    mkdir -p ${LCG_destdir}/../share
+fi
+
+cd ${LCG_destdir}/../share
+for data in $(echo ${LCG_datafiles} | sed 's/;/ /g') ; do
+    data_dir=$(tar tzf ${LCG_tardir}/${data} | head -1)
+    if [ ! -d ${data_dir} ]; then
+	tar xvzf ${LCG_tardir}/${data}
+    fi
+done 
+
 # If the source hasn't been unpacked, then unpack it.  This check
 # helps make sure that redoing "make pkg_config" doesn't force a
 # rebuilt.
+cd ${LCG_destdir}
 if [ ! -d ${LOCAL_src} -a -f  ${LCG_tardir}/${LCG_tarfilename} ]; then 
     tar xvfz ${LCG_tardir}/${LCG_tarfilename}
+else
+    echo XXXX ${LCG_src} already exists. 
+    echo XXXX Not unpacking and reconfiguring.  
+    echo XXXX Remove directory to force reconfiguration.  
+    exit 0
 fi
-
-# Unpack the geant4 data files
-cd ${LCG_destdir}/..
-if [ ! -d share ]; then
-    mkdir share
-fi
-cd share
-
-for data in $(echo ${LCG_datafiles} | sed 's/;/ /g') ; do
-    tar xvzf ${LCG_tardir}/${data}
-done 
 
 cd ${LOCAL_build}
 cmake -DCMAKE_INSTALL_PREFIX=${LOCAL_install} ${LCG_build_options} ${LOCAL_src}
--- LCG_Builders/gsl/scripts/gsl_install.sh
+++ LCG_Builders/gsl/scripts/gsl_install.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 cd ${LCG_destbindir}/gsl-${LCG_package_config_version}
+
+if [ -d ../bin -a -d ../lib -a -d ../include -a -d ../share ]; then
+    echo XXXX No need to install gsl-${LCG_package_config_version}
+    echo XXXX Remove installation to force re-installation
+    exit 0
+fi
+
 make install

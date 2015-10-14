0f8b2d9ce36281f2455d685613a2aaa0f25637b0
Clark McGrew <clark.mcgrew@stonybrook.edu>
2015-10-14 19:30:40 -0400

    Add option for multi-core compilation.

--- LCG_Builders/geant4/scripts/geant4_make.sh
+++ LCG_Builders/geant4/scripts/geant4_make.sh
@@ -3,7 +3,16 @@
 # Script to handle a cmake configured package.  This has been
 # customized for geant4.
 #
+
+# Set the maximum load during the job.  This can be overridden in the
+# environment.
+if [ "x$LCG_MAX_LOAD" = "x" ]; then
+    LCG_MAX_LOAD=1.0
+fi
+echo Limit load to ${LCG_MAX_LOAD}
+
 # Go to the build directory and run make.  The build directory must
 # match LOCAL_build in the config script.
 cd ${LCG_destbindir}/build
-make
+make -l ${LCG_MAX_LOAD} -j
+

522cfea7789df81d360e29bef105933a6f653a32
Clark McGrew <clark.mcgrew@stonybrook.edu>
2015-10-16 11:07:41 -0400

    Use ROOT 6.04.06 and a cmake build.

--- LCG_Builders/ROOT/scripts/ROOT_config.sh
+++ LCG_Builders/ROOT/scripts/ROOT_config.sh
@@ -1,42 +1,53 @@
 #!/bin/sh
-
-# This config script is aimed at an autoconf package.  It should be
+# This config script is aimed at a cmake package.  It should be
 # customized for the package being built.
 
 # This should be customized to here the package should be built.  The
-# default is to build in the LCG_destbindir so that the build
+# default is to install in the LCG_destbindir so that the build
 # directory is near the installation directory.  It needs to be the
 # LCG_destbindir since many autoconf and related packages compile in
-# the source directories.  Make sure the destination exists, and then
-# go there.
-mkdir -p ${LCG_destbindir}
-cd ${LCG_destbindir}
+# the source directories.  Make sure the destination exists.
+mkdir -p ${LCG_destdir}
+
+# This needs to be set to the installation prefix
+LOCAL_install=${LCG_destbindir}
+mkdir -p ${LOCAL_install}
 
 # This has to be set to the directory that the tar file will unpack
-# into.  If upstream changes the top-level tar directory, this will
-# need to be changed.
-LOCAL_srcdir=${PWD}/${LCG_srcdir}
+# into when tar is run from the ${LCG_destdir} directory.  If upstream
+# changes the top-level tar directory, this will need to be changed.
+LOCAL_src=${LCG_destdir}/${LCG_srcdir}
 
 # If the source hasn't been unpacked, then unpack it.  This check
 # helps make sure that redoing "make pkg_config" doesn't force a
 # rebuilt.
-if [ ! -d ${LOCAL_srcdir} -a -f  ${LCG_tardir}/${LCG_tarfilename} ]; then 
-	tar xvfz ${LCG_tardir}/${LCG_tarfilename}
+cd ${LCG_destdir}
+if [ ! -d ${LOCAL_src} -a -f  ${LCG_tardir}/${LCG_tarfilename} ]; then 
+    tar xvfz ${LCG_tardir}/${LCG_tarfilename}
+else
+    echo XXXX ${LCG_src} already exists, so not unpacking.
+    echo XXXX Remove directory to force new source.
 fi
 
-cd ${LOCAL_srcdir}
-if [ -f config.status ]; then
-    echo "WARNING: ${LCG_package} already configured."
-    echo "WARNING: Remove ${PWD}/config.status to reconfig."
+# This needs to be set to where the package will be built
+LOCAL_build=${LOCAL_src}-build
+mkdir -p ${LOCAL_build}
+
+# Make sure the ROOTSYS variable isn't set for the build.
+if [ ${#ROOTSYS} != 0 ]; then
+    unset ROOTSYS
+fi
+	   
+# Go to the build area.
+cd ${LOCAL_build}
+
+if [ -e CMakeCache.txt -a CMakeCache.txt \
+	-nt ${LCG_package_root}/cmt/requirements ]; then
+    echo XXXX No need to rerun configuration
     exit 0
 fi
+exit 0
 
-COVLD=""
-#If coverage testing is configured, configure ROOT with differend
-#linker. This linker just calls g++ with lgcov at the end.
-if [[ ! -z "$GCOV_TOOL" ]];then
-	COVLD="--with-ld=${LCG_package_root}/scripts/root_gcovld.sh"
-fi
+cmake -DCMAKE_INSTALL_PREFIX=${LOCAL_install} ${LCG_build_options} ${LOCAL_src}
 
-# Run configure
-eval "./configure ${LCG_ROOT_CONFIG_ARCH} ${LCG_ROOT_CONFIG_OPTIONS} ${COVLD}"
+
--- LCG_Builders/ROOT/scripts/ROOT_install.sh
+++ LCG_Builders/ROOT/scripts/ROOT_install.sh
@@ -1,5 +1,21 @@
-#!/bin/sh
+#
+# Script to handle a cmake configured package.  This has been
+# customized for ROOT.
 
-cd ${LCG_destbindir}/${LCG_srcdir}
-BINDIR=${LCG_destbindir} make install
+# This has to be set to the directory that the tar file will unpack
+# into when tar is run from the ${LCG_destdir} directory.  If upstream
+# changes the top-level tar directory, this will need to be changed.
+LOCAL_src=${LCG_destdir}/${LCG_srcdir}
+
+# This needs to be set to where the package will be built
+LOCAL_build=${LOCAL_src}-build
+
+# Make sure the ROOTSYS variable isn't set for the build.
+if [ ${#ROOTSYS} != 0 ]; then
+    unset ROOTSYS
+fi
+	   
+# Go to the build directory and run make install.
+cd ${LOCAL_build}
+cmake --build ${LOCAL_build} -- install
 

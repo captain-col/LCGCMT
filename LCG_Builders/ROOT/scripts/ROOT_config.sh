bc93a8d897e83b3abd57f4d4c6cc7a45b5902edf
Clark McGrew <clark.mcgrew@stonybrook.edu>
2015-11-04 11:48:55 -0500

    Use the CAPTAIN version of cmake to build LCGCMT.

--- LCG_Builders/ROOT/scripts/ROOT_config.sh
+++ LCG_Builders/ROOT/scripts/ROOT_config.sh
@@ -2,34 +2,39 @@
 # This config script is aimed at a cmake package.  It should be
 # customized for the package being built.
 
-# This should be customized to here the package should be built.  The
-# default is to install in the LCG_destbindir so that the build
-# directory is near the installation directory.  It needs to be the
-# LCG_destbindir since many autoconf and related packages compile in
-# the source directories.  Make sure the destination exists.
+# This should be customized to the base directory for the package
+# installation. All of the version specific files will be rooted from
+# here.
 mkdir -p ${LCG_destdir}
 
-# This needs to be set to the installation prefix
+# The location of the machine specific installation directory.  This
+# is usually set to LCG_destbindir, and must exist.
 LOCAL_install=${LCG_destbindir}
 mkdir -p ${LOCAL_install}
 
 # This has to be set to the directory that the tar file will unpack
-# into when tar is run from the ${LCG_destdir} directory.  If upstream
-# changes the top-level tar directory, this will need to be changed.
+# into when tar is run.  Tar is usually run from ${LCG_destdir}
+# directory, but if the compilation modifies the source directory,
+# this needs to be ${LCG_destbindir}.  If upstream changes the
+# top-level tar directory, this will need to be changed.  In the case
+# of some builder that modify the source directory, this should be
+# inside the machine specific directory.
 LOCAL_src=${LCG_destdir}/${LCG_srcdir}
 
 # If the source hasn't been unpacked, then unpack it.  This check
 # helps make sure that redoing "make pkg_config" doesn't force a
-# rebuilt.
+# rebuilt.  Change directory to the right directory to get the source
+# unpacked into LOCAL_src (usually LCG_destdir, or LCG_destbindir).
 cd ${LCG_destdir}
 if [ ! -d ${LOCAL_src} -a -f  ${LCG_tardir}/${LCG_tarfilename} ]; then 
     tar xvfz ${LCG_tardir}/${LCG_tarfilename}
 else
-    echo XXXX ${LCG_src} already exists, so not unpacking.
+    echo XXXX ${LOCAL_src} already exists, so not unpacking.
     echo XXXX Remove directory to force new source.
 fi
 
-# This needs to be set to where the package will be built
+# This needs to be set to where the package will be built.  This might
+# be the same as the source directory.
 LOCAL_build=${LOCAL_src}-build
 mkdir -p ${LOCAL_build}
 
--- LCG_Builders/boost/scripts/boost_config.sh
+++ LCG_Builders/boost/scripts/boost_config.sh
@@ -1,21 +1,40 @@
 #!/bin/sh
 
-package_directory=boost_${LCG_file_config_version}
+# This should be customized to the base directory for the package
+# installation. All of the version specific files will be rooted from
+# here.
+mkdir -p ${LCG_destdir}
 
-mkdir -p ${LCG_destbindir}
+# The location of the machine specific installation directory.  This
+# is usually set to LCG_destbindir, and must exist.
+LOCAL_install=${LCG_destbindir}
+mkdir -p ${LOCAL_install}
+
+# This has to be set to the directory that the tar file will unpack
+# into when tar is run.  Tar is usually run from ${LCG_destdir}
+# directory, but if the compilation modifies the source directory,
+# this needs to be ${LCG_destbindir}.  If upstream changes the
+# top-level tar directory, this will need to be changed.  In the case
+# of some builder that modify the source directory, this should be
+# inside the machine specific directory.
+LOCAL_src=${LCG_destbindir}/boost_${LCG_file_config_version}
+
+# If the source hasn't been unpacked, then unpack it.  This check
+# helps make sure that redoing "make pkg_config" doesn't force a
+# rebuilt.  Change directory to the right directory to get the source
+# unpacked into LOCAL_src (usually LCG_destdir, or LCG_destbindir).
 cd ${LCG_destbindir}
-
-if [ -d ${package_directory} ]; then
-    echo XXXX ${LCG_destbindir}/${package_directory} already exists. 
-    echo XXXX Not unpacking and reconfiguring.  
-    echo XXXX Remove directory to force reconfiguration.  
-    exit 0
+if [ ! -d ${LOCAL_src} -a -f  ${LCG_tardir}/${LCG_tarfilename} ]; then 
+    tar xvfz ${LCG_tardir}/${LCG_tarfilename}
+else
+    echo XXXX ${LCG_src} already exists, so not unpacking.
+    echo XXXX Remove directory to force new source.
 fi
 
-tar xvfz ${LCG_tardir}/${LCG_tarfilename}
-cd ${package_directory}
+# This needs to be set to where the package will be built.  This might
+# be the same as the source directory.
+LOCAL_build=${LOCAL_src}
+mkdir -p ${LOCAL_build}
 
-#./bootstrap.sh --prefix=${LCG_destdir} \
-#    --without-libraries=${LCG_boost_with_libraries}
-
-./bootstrap.sh  --prefix=${LCG_destdir}
+cd ${LOCAL_build}
+./bootstrap.sh  --prefix=${LOCAL_install}

5488e8e6155ff0a94a0c3d87278c980d0ef43cd0
Clark McGrew <clark.mcgrew@stonybrook.edu>
2015-11-04 11:47:02 -0500

    Complete implementation of the cmake builder and interface packages.
    It looks like CAPTAIN is going to need to provide it's own version of cmake
    since SL5/6 still exists, and uses a very old version.  CMake can now be
    added to the build.

--- LCG_Builders/cmake/scripts/cmake_config.sh
+++ LCG_Builders/cmake/scripts/cmake_config.sh
@@ -1,7 +1,40 @@
 #!/bin/sh
 
-cd ${LCG_builddir}
-tar xvfz ${LCG_tarfilename}
-cd cmake-${LCG_package_config_version}
-eval "${LCG_cmake_compile_options}"
-./configure --prefix=${LCG_extdir}/CMake/${LCG_package_config_version}/${LCG_CMTCONFIG}
+# This should be customized to the base directory for the package
+# installation. All of the version specific files will be rooted from
+# here.
+mkdir -p ${LCG_destdir}
+
+# The location of the machine specific installation directory.  This
+# is usually set to LCG_destbindir, and must exist.
+LOCAL_install=${LCG_destbindir}
+mkdir -p ${LOCAL_install}
+
+# This has to be set to the directory that the tar file will unpack
+# into when tar is run.  Tar is usually run from ${LCG_destdir}
+# directory, but if the compilation modifies the source directory,
+# this needs to be ${LCG_destbindir}.  If upstream changes the
+# top-level tar directory, this will need to be changed.  In the case
+# of some builder that modify the source directory, this should be
+# inside the machine specific directory.
+LOCAL_src=${LCG_destbindir}/cmake-${LCG_package_config_version}
+
+# If the source hasn't been unpacked, then unpack it.  This check
+# helps make sure that redoing "make pkg_config" doesn't force a
+# rebuilt.  Change directory to the right directory to get the source
+# unpacked into LOCAL_src (usually LCG_destdir, or LCG_destbindir).
+cd ${LCG_destbindir}
+if [ ! -d ${LOCAL_src} -a -f  ${LCG_tardir}/${LCG_tarfilename} ]; then 
+    tar xvfz ${LCG_tardir}/${LCG_tarfilename}
+else
+    echo XXXX ${LOCAL_src} already exists, so not unpacking.
+    echo XXXX Remove directory to force new source.
+fi
+
+# This needs to be set to where the package will be built.  This might
+# be the same as the source directory.
+LOCAL_build=${LOCAL_src}
+mkdir -p ${LOCAL_build}
+
+cd ${LOCAL_build}
+./bootstrap --prefix=${LOCAL_install}
--- LCG_Builders/cmake/scripts/cmake_make.sh
+++ LCG_Builders/cmake/scripts/cmake_make.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-cd ${LCG_builddir}/cmake-${LCG_package_config_version}
+# This has to be set to the directory that the tar file was unpacked.
+# If upstream changes the top-level tar directory, this will need to
+# be changed.  In the case of some builder that modify the source
+# directory, this should be inside the machine specific directory.
+LOCAL_src=${LCG_destbindir}/cmake-${LCG_package_config_version}
+
+# This needs to be set to where the package will be built.  This might
+# be the same as the source directory.
+LOCAL_build=${LOCAL_src}
+
+cd ${LOCAL_build}
 make

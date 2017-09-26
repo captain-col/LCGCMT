73f2c21447ed3950b955790ea4bb0d12071566ea
Clark McGrew <clark.mcgrew@stonybrook.edu>
2017-09-26 14:13:10 -0400

    Clean up the boost build files so that it rebuilds less.

--- LCG_Builders/boost/scripts/boost_config.sh
+++ LCG_Builders/boost/scripts/boost_config.sh
@@ -3,17 +3,13 @@
 # This should be customized to the base directory for the package
 # installation. All of the version specific files will be rooted from
 # here.
-mkdir -p ${LCG_destdir}
+LOCAL_dest=${LCG_destdir}
+mkdir -p ${LOCAL_dest}
 
 # The location of the machine specific installation directory.  This
 # is usually set to LCG_destbindir, and must exist.
 LOCAL_install=${LCG_destbindir}
 mkdir -p ${LOCAL_install}
-
-# For boost, make sure the cmt config has been run.
-if [ "x$LCG_file_config_version" = "x" ]; then
-    exit 1;
-fi
 
 # This has to be set to the directory that the tar file will unpack
 # into when tar is run.  Tar is usually run from ${LCG_destdir}
@@ -22,13 +18,26 @@ fi
 # top-level tar directory, this will need to be changed.  In the case
 # of some builder that modify the source directory, this should be
 # inside the machine specific directory.
-LOCAL_src=${LCG_destbindir}/boost_${LCG_file_config_version}
+LOCAL_src=${LOCAL_dest}/boost_${LCG_file_config_version}
+
+# This needs to be set to where the package will be built.  For some
+# packages that modify the source directory, this might be the same as
+# the source directory, but it's usually "parallel" to the source with
+# a suffix "-build".
+# BOOST NEEDS TO BE BUILT IN THE SRC DIRECTORY!!!!!!!
+LOCAL_build=${LOCAL_src}
+
+# MAKE SURE THAT THE LCG_Builders/boost/setup.sh script was run
+if [ "x${LCG_file_config_version}" = "x" ]; then
+    echo The local cmt setup must have been run.
+    exit 1
+fi
 
 # If the source hasn't been unpacked, then unpack it.  This check
 # helps make sure that redoing "make pkg_config" doesn't force a
 # rebuilt.  Change directory to the right directory to get the source
 # unpacked into LOCAL_src (usually LCG_destdir, or LCG_destbindir).
-cd ${LCG_destbindir}
+cd ${LOCAL_dest}
 if [ ! -d ${LOCAL_src} -a -f  ${LCG_tardir}/${LCG_tarfilename} ]; then 
     tar xvfz ${LCG_tardir}/${LCG_tarfilename}
 else
@@ -36,12 +45,8 @@ else
     echo XXXX Remove directory to force new source.
 fi
 
-# This needs to be set to where the package will be built.  This might
-# be the same as the source directory.
-LOCAL_build=${LOCAL_src}
 mkdir -p ${LOCAL_build}
-
 cd ${LOCAL_build}
-if [ ! -x b2 -o ! -x bjam ]; then
-    ./bootstrap.sh  --prefix=${LOCAL_install}
+if [ ! -x ${LCG_bjam_bin} ]; then
+    ${LOCAL_src}/bootstrap.sh  --prefix=${LOCAL_install}
 fi
--- LCG_Builders/boost/scripts/boost_install.sh
+++ LCG_Builders/boost/scripts/boost_install.sh
@@ -1,6 +1,37 @@
 #!/bin/sh
 
-cd ${LCG_destbindir}/boost_${LCG_file_config_version}
+# This should be customized to the base directory for the package
+# installation. All of the version specific files will be rooted from
+# here.
+LOCAL_dest=${LCG_destdir}
+
+# The location of the machine specific installation directory.  This
+# is usually set to LCG_destbindir, and must exist.
+LOCAL_install=${LCG_destbindir}
+
+# This has to be set to the directory that the tar file will unpack
+# into when tar is run.  Tar is usually run from ${LCG_destdir}
+# directory, but if the compilation modifies the source directory,
+# this needs to be ${LCG_destbindir}.  If upstream changes the
+# top-level tar directory, this will need to be changed.  In the case
+# of some builder that modify the source directory, this should be
+# inside the machine specific directory.
+LOCAL_src=${LOCAL_dest}/boost_${LCG_file_config_version}
+
+# This needs to be set to where the package will be built.  For some
+# packages that modify the source directory, this might be the same as
+# the source directory, but it's usually "parallel" to the source with
+# a suffix "-build".
+# BOOST NEEDS TO BE BUILT IN THE SRC DIRECTORY!!!!!!!
+LOCAL_build=${LOCAL_src}
+
+# MAKE SURE THAT THE LCG_Builders/boost/setup.sh script was run
+if [ "x${LCG_file_config_version}" = "x" ]; then
+    echo The local cmt setup must have been run.
+    exit 1
+fi
+
+cd ${LOCAL_build}
 
 ${LCG_bjam_bin} ${LCG_boost_jam_opts} ${LCG_boost_compile_options} \
     --toolset=${LCG_boost_toolset} \

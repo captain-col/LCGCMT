01114316c41e7a16615812ba55bbd95bfd312ebc
Clark McGrew <clark.mcgrew@stonybrook.edu>
2017-09-26 09:27:56 -0400

    Fix the cmake build so that it uses the right directories.

--- LCG_Builders/cmake/scripts/cmake_config.sh
+++ LCG_Builders/cmake/scripts/cmake_config.sh
@@ -3,12 +3,11 @@
 # This should be customized to the base directory for the package
 # installation. All of the version specific files will be rooted from
 # here.
-mkdir -p ${LCG_destdir}
+LOCAL_dest=${LCG_destdir}
 
 # The location of the machine specific installation directory.  This
 # is usually set to LCG_destbindir, and must exist.
 LOCAL_install=${LCG_destbindir}
-mkdir -p ${LOCAL_install}
 
 # This has to be set to the directory that the tar file will unpack
 # into when tar is run.  Tar is usually run from ${LCG_destdir}
@@ -17,13 +16,23 @@ mkdir -p ${LOCAL_install}
 # top-level tar directory, this will need to be changed.  In the case
 # of some builder that modify the source directory, this should be
 # inside the machine specific directory.
-LOCAL_src=${LCG_destbindir}/cmake-${LCG_package_config_version}
+LOCAL_src=${LOCAL_dest}/cmake-${LCG_package_config_version}
+
+# This needs to be set to where the package will be built.  For some
+# packages that modify the source directory, this might be the same as
+# the source directory, but it's usually "parallel" to the source with
+# a suffix "-build".
+LOCAL_build=${LOCAL_src}-build
+
+mkdir -p ${LOCAL_dest}
+mkdir -p ${LOCAL_install}
+mkdir -p ${LOCAL_build}
 
 # If the source hasn't been unpacked, then unpack it.  This check
 # helps make sure that redoing "make pkg_config" doesn't force a
 # rebuilt.  Change directory to the right directory to get the source
 # unpacked into LOCAL_src (usually LCG_destdir, or LCG_destbindir).
-cd ${LCG_destbindir}
+cd ${LOCAL_dest}
 if [ ! -d ${LOCAL_src} -a -f  ${LCG_tardir}/${LCG_tarfilename} ]; then 
     tar xvfz ${LCG_tardir}/${LCG_tarfilename}
 else
@@ -31,12 +40,6 @@ else
     echo XXXX Remove directory to force new source.
 fi
 
-# This needs to be set to where the package will be built.  This might
-# be the same as the source directory.
-LOCAL_build=${LOCAL_src}
-mkdir -p ${LOCAL_build}
-
 cd ${LOCAL_build}
-if [ ! -f Makefile ]; then
-    ./bootstrap --prefix=${LOCAL_install}
-fi
+
+${LOCAL_src}/bootstrap --prefix=${LOCAL_install}
